ea1e0929a592cb16762d3fa59e0112e4a525035b
mugongpun <p2r2o3@gmail.com>
2025-01-14 01:11:48 +0900

    [feat] #1 엔티티 생성

--- src/main/java/com/example/restapi/RestapiApplication.java
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -2,8 +2,10 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RestapiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/restapi/entity/member/Member.java
@@ -0,0 +1,54 @@
+package com.example.restapi.entity.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(value = {AuditingEntityListener.class})
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String pwd;
+
+    private String name;
+
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime joinDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    //일반회원, 관리자
+    private String role;
+
+    public void changePassword(String password) {
+        this.pwd = password;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public void changeEmail(String email) {
+        this.email = email;
+    }
+
+    public void changeRole(String role) {
+        this.role = role;
+    }
+}
