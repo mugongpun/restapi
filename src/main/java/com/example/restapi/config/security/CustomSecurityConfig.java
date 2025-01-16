3477d450c5c92ae8dddb25af2169ed15ae68001b
mugongpun <p2r2o3@gmail.com>
2025-01-16 12:57:08 +0900

    [feat] #9 시큐리티 설정 추가

--- src/main/java/com/example/restapi/config/security/CustomSecurityConfig.java
+++ src/main/java/com/example/restapi/config/security/CustomSecurityConfig.java
@@ -1,15 +1,29 @@
 package com.example.restapi.config.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Slf4j
 public class CustomSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable());
+        httpSecurity.logout(config -> config.disable());
+        httpSecurity.csrf(config -> config.disable());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER));
+        return httpSecurity.build();
     }
 }
