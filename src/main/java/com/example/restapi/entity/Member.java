edc823dd467dc88b0ef96b6a6910032f8f9a7c44
mugongpun <p2r2o3@gmail.com>
2025-01-22 13:40:22 +0900

    [fix] member엔티티 수정

---
+++ src/main/java/com/example/restapi/entity/Member.java
@@ -0,0 +1,57 @@
+package com.example.restapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(value = {AuditingEntityListener.class})
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String mid;
+
+    private String pwd;
+
+    private String name;
+
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime joinDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    //일반회원, 관리자
+    private String role;
+
+    public void changePassword(String password) {
+        this.pwd = password;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public void changeEmail(String email) {
+        this.email = email;
+    }
+
+    public void changeRole(String role) {
+        this.role = role;
+    }
+}
