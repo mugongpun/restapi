ba4f30a6da1ee016473227a39e96ef17f4bb025e
mugongpun <p2r2o3@gmail.com>
2025-02-02 00:55:16 +0900

    [fix] #32 Product엔티티 수정

--- src/main/java/com/example/restapi/entity/Product.java
+++ src/main/java/com/example/restapi/entity/Product.java
@@ -1,21 +1,23 @@
 package com.example.restapi.entity;
 
 import com.example.restapi.exception.OrderTaskException;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Product {
 
     @Id
     @GeneratedValue
     private Long id;
+    @Column(name = "product_name", unique = true)
     private String name;
     private int price;
     private int stockQuantity;
@@ -27,14 +29,18 @@ public class Product {
     public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
-            throw OrderTaskException.Exceptions.NOT_ENOUGH_STOCK_EXCEPTION.value();
+            throw OrderTaskException.Exceptions.NOT_ENOUGH_STOCK_EXCEPTION.createException();
         }
         this.stockQuantity = restStock;
     }
 
+    public void changeStock(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
     public void changeProduct(int price, String name, int stockQuantity) {
         if (price <= 0) {
-            throw OrderTaskException.Exceptions.NEGATIVE_PRICE_EXCEPTION.value();
+            throw OrderTaskException.Exceptions.NEGATIVE_PRICE_EXCEPTION.createException();
         }
         this.price = price;
         this.name = name;
@@ -43,9 +49,9 @@ public class Product {
 
     public static Product createProduct(String name, int price, int stockQuantity) {
         Product product = new Product();
+        product.name = name;
         product.stockQuantity = stockQuantity;
         product.price = price;
-        product.name = name;
         return product;
     }
 }
