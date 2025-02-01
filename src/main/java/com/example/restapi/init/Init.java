4a2dd7ac47d072ccbab592cb1daa03126c038ad4
mugongpun <p2r2o3@gmail.com>
2025-02-02 01:00:56 +0900

    [feat] 시작 데이터 추가(추후 삭제예정)

---
+++ src/main/java/com/example/restapi/init/Init.java
@@ -0,0 +1,57 @@
+package com.example.restapi.init;
+
+import com.example.restapi.entity.Member;
+import com.example.restapi.entity.Product;
+import com.example.restapi.repository.MemberRepository;
+import com.example.restapi.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class Init {
+
+    private final InitService initService;
+
+    @PostConstruct
+    @Transactional
+    public void init() {
+        initService.init();
+    }
+
+
+    @Component
+    @RequiredArgsConstructor
+    @Transactional
+    private static class InitService{
+        private final ProductRepository productRepository;
+        private final MemberRepository memberRepository;
+        private final PasswordEncoder passwordEncoder;
+
+        public void init() {
+
+            for (int i = 0; i < 30; i++) {
+                Product product = Product.createProduct("상품번호" + (i + 1), 1000 + i + 1, 30);
+                productRepository.save(product);
+            }
+
+            //given(이런 데이터가 주어지면)
+            for (int i =1; i <= 10; i++) {
+                Member member = Member.builder()
+                                      .mid("user" + i)
+                                      .pwd(passwordEncoder.encode("1111"))
+                                      .name("user" + i)
+                                      .email("user" + i + "@aaa.com")
+                                      .role("user")
+                                      .build();
+                memberRepository.save(member);
+            }
+
+
+        }
+    }
+
+}
