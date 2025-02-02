ba691b8696aafc8ca0cea0701e6f7611d660a307
mugongpun <p2r2o3@gmail.com>
2025-02-02 19:38:10 +0900

    [fix] #48 ProdudctService changeProduct 메서드수정

--- src/main/java/com/example/restapi/service/ProductService.java
+++ src/main/java/com/example/restapi/service/ProductService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,30 +30,42 @@ public class ProductService {
     //상품 재고 수정
     public ProductDTO changeProductStock(Long productId, int quantity) {
         Optional<Product> byId = productRepository.findById(productId);
-        Product findProduct = byId.orElseThrow(() -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get());
+        Product findProduct = byId.orElseThrow(
+                () -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get()
+        );
         findProduct.changeStock(quantity);
         return new ProductDTO(findProduct);
     }
 
     //상품 수정
-    public ProductDTO changeProduct(ProductDTO productDTO) {
-        if (!productRepository.existsByName(productDTO.getName())) {
-            throw ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get();
-        }
-        Product findProduct = productRepository.findByName(productDTO.getName())
-                                           .get();
-        return new ProductDTO(findProduct);
+    public ProductDTO changeProduct(Long id, ProductDTO productDTO) {
+        Product product = productRepository.findById(id)
+                                           .orElseThrow(
+                                                   () -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get()
+                                           );
+
+        product.changeProduct(productDTO.getPrice(), productDTO.getName(), productDTO.getStockQuantity());
+
+        return new ProductDTO(product);
     }
 
     public void deleteProduct(Long productId) {
         Product findProduct = productRepository.findById(productId)
-                                               .orElseThrow(() -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get());
+                                               .orElseThrow(
+                                                       () -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get());
         productRepository.delete(findProduct);
     }
 
     public void deleteProduct(String productName) {
         Product findProduct = productRepository.findByName(productName)
-                                           .orElseThrow(() -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get());
+                                               .orElseThrow(
+                                                       () -> ProductTaskException.Exceptions.NOT_EXIST_PRODUCT.get()
+                                               );
         productRepository.delete(findProduct);
+    }
+
+    public List<Product> findAll() {
+        List<Product> productList = productRepository.findAll();
+        return productList;
     }
 }
