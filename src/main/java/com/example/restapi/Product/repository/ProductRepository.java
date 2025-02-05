1302e7b30e0632014dd3e6d0e7aa65ab74889a25
mugongpun <p2r2o3@gmail.com>
2025-02-05 17:46:40 +0900

    [fix] #59 ProductRepository 수정

--- src/main/java/com/example/restapi/Product/repository/ProductRepository.java
+++ src/main/java/com/example/restapi/Product/repository/ProductRepository.java
@@ -2,12 +2,17 @@ package com.example.restapi.Product.repository;
 
 import com.example.restapi.Product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name);
 
     boolean existsByName(String name);
+
+    @Query("select p from Product p")
+    List<Product> findAllProduct();
 
 }
