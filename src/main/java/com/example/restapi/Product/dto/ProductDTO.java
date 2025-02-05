b470f9accb35499b316fa4f9d56f6830df4ca233
mugongpun <p2r2o3@gmail.com>
2025-02-05 17:44:27 +0900

    [fix] #58 ProductDTO 수정

--- src/main/java/com/example/restapi/Product/dto/ProductDTO.java
+++ src/main/java/com/example/restapi/Product/dto/ProductDTO.java
@@ -1,13 +1,22 @@
 package com.example.restapi.Product.dto;
 
 import com.example.restapi.Product.domain.Product;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class ProductDTO {
     private String name;
+    @Min(value = 1, message = "최소 1원 이상이여야 합니다")
+    @Max(value = Integer.MAX_VALUE, message = "가격은 최대값을")
     private int price;
+    @Min(value = 0,message = "최소 수량은 0개 입니다")
+    @Max(value = 99999, message = "최대 수량은 99999개 입니다")
     private int stockQuantity;
+    private List<String> fileNames;
 
     public ProductDTO() {
     }
@@ -16,5 +25,9 @@ public class ProductDTO {
         this.name = product.getName();
         this.price = product.getPrice();
         this.stockQuantity = product.getStockQuantity();
+        this.fileNames = product.getProductImages()
+                                .stream()
+                                .map(productImage -> productImage.getFileName())
+                                .toList();
     }
 }
