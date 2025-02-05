0ef2bf9d8ab5e02c1dafa865ddc82edb65c125ba
mugongpun <p2r2o3@gmail.com>
2025-02-05 17:32:57 +0900

    [fix] #57 Product 엔티티 수정

--- src/main/java/com/example/restapi/Product/domain/Product.java
+++ src/main/java/com/example/restapi/Product/domain/Product.java
@@ -1,11 +1,12 @@
 package com.example.restapi.Product.domain;
 
 import com.example.restapi.global.exception.OrderTaskException;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.example.restapi.ProductImage.domain.ProductImage;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,10 +18,17 @@ public class Product {
     @Id
     @GeneratedValue
     private Long id;
+
     @Column(name = "product_name", unique = true)
     private String name;
+
     private int price;
+
     private int stockQuantity;
+
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductImage> productImages = new ArrayList<>();
+
 
     public void addStock(int count) {
         this.stockQuantity += count;
@@ -46,6 +54,38 @@ public class Product {
         this.name = name;
         this.stockQuantity = stockQuantity;
     }
+
+
+    public void changeProduct(int price, String name, int stockQuantity,List<ProductImage> productImages) {
+        if (price <= 0) {
+            throw OrderTaskException.Exceptions.NEGATIVE_PRICE_EXCEPTION.get();
+        }
+        this.price = price;
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+        for (ProductImage productImage : productImages) {
+            this.addImages(productImage);
+        }
+    }
+
+    //양방향 관계
+    public void addImages(ProductImage productImage) {
+        productImages.add(productImage);
+        productImage.changeProduct(this);
+        productImage.changeIdx(productImages.size());
+    }
+
+    public static Product createProduct(String name, int price, int stockQuantity, List<ProductImage> productImages) {
+        Product product = new Product();
+        product.name = name;
+        product.stockQuantity = stockQuantity;
+        product.price = price;
+        for (ProductImage productImage : productImages) {
+            product.addImages(productImage);
+        }
+        return product;
+    }
+
 
     public static Product createProduct(String name, int price, int stockQuantity) {
         Product product = new Product();
